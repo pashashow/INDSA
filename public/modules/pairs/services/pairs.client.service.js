e1e77e89eed9c250495e7c0386fd82e3ccfeb861
pashashow <pavel@kapelson.com>
2015-08-10 21:55:31 +0300

    Warnings prcessing

--- public/modules/pairs/services/pairs.client.service.js
+++ public/modules/pairs/services/pairs.client.service.js
@@ -2,9 +2,9 @@
 
 //Pairs service used to communicate Pairs REST endpoints
 angular.module('pairs').factory('Pairs', ['$resource',
-	function($resource) {
+	function ($resource) {
 		return $resource('pairs/:pairId', { pairId: '@_id'
-		}, {
+		},{
 			update: {
 				method: 'PUT'
 			}
