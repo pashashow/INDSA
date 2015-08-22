36aaef3e3aeea26df8ae6f9f3d9bfcde85411db9
Pavel Kapelson <pavel@kapelson.com>
2015-08-22 18:47:53 +0300

    [lite-fixes] source code re-styling
    Branch: [lite-fixes]

--- public/modules/users/controllers/settings.client.controller.js
+++ public/modules/users/controllers/settings.client.controller.js
@@ -1,14 +1,18 @@
 'use strict';
 
-angular.module('users').controller('SettingsController', ['$scope', '$http', '$location', 'Users', 'Authentication',
-	function($scope, $http, $location, Users, Authentication) {
+angular.module('users')
+.controller('SettingsController',
+    ['$scope', '$http', '$location', 'Users', 'Authentication',
+	function ($scope, $http, $location, Users, Authentication) {
 		$scope.user = Authentication.user;
 
 		// If user is not signed in then redirect back home
-		if (!$scope.user) $location.path('/');
+		if (!$scope.user){
+            $location.path('/');
+        }
 
 		// Check if there are additional accounts 
-		$scope.hasConnectedAdditionalSocialAccounts = function(provider) {
+		$scope.hasConnectedAdditionalSocialAccounts = function (provider) {
 			for (var i in $scope.user.additionalProvidersData) {
 				return true;
 			}
@@ -17,19 +21,21 @@ angular.module('users').controller('SettingsController', ['$scope', '$http', '$l
 		};
 
 		// Check if provider is already in use with current user
-		$scope.isConnectedSocialAccount = function(provider) {
-			return $scope.user.provider === provider || ($scope.user.additionalProvidersData && $scope.user.additionalProvidersData[provider]);
+		$scope.isConnectedSocialAccount = function (provider) {
+			return $scope.user.provider === provider ||
+                   ($scope.user.additionalProvidersData &&
+                    $scope.user.additionalProvidersData[provider]);
 		};
 
 		// Remove a user social account
-		$scope.removeUserSocialAccount = function(provider) {
+		$scope.removeUserSocialAccount = function (provider) {
 			$scope.success = $scope.error = null;
 
 			$http.delete('/users/accounts', {
 				params: {
 					provider: provider
 				}
-			}).success(function(response) {
+			}).success(function (response) {
 				// If successful show success message and clear form
 				$scope.success = true;
 				$scope.user = Authentication.user = response;
@@ -39,12 +45,12 @@ angular.module('users').controller('SettingsController', ['$scope', '$http', '$l
 		};
 
 		// Update a user profile
-		$scope.updateUserProfile = function(isValid) {
+		$scope.updateUserProfile = function (isValid) {
 			if (isValid) {
 				$scope.success = $scope.error = null;
 				var user = new Users($scope.user);
 
-				user.$update(function(response) {
+				user.$update(function (response) {
 					$scope.success = true;
 					Authentication.user = response;
 				}, function(response) {
@@ -59,11 +65,13 @@ angular.module('users').controller('SettingsController', ['$scope', '$http', '$l
 		$scope.changeUserPassword = function() {
 			$scope.success = $scope.error = null;
 
-			$http.post('/users/password', $scope.passwordDetails).success(function(response) {
+			$http.post('/users/password', $scope.passwordDetails)
+            .success(function (response) {
 				// If successful show success message and clear form
 				$scope.success = true;
 				$scope.passwordDetails = null;
-			}).error(function(response) {
+			})
+            .error(function (response) {
 				$scope.error = response.message;
 			});
 		};
